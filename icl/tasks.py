FIX dMMSE baseline computation batching for high M

icl/tasks.py
@@ -2,7 +2,9 @@ from typing import Generic, TypeVar
 
 import torch
 
+
 T = TypeVar('T', 'GaussianTaskDistribution', 'DiscreteTaskDistribution', 'SingletonTaskDistribution')
+
 
 class RegressionSequenceDistribution(Generic[T]):
     """
@@ -82,7 +84,8 @@ class RegressionSequenceDistribution(Generic[T]):
         ys = xs @ ws.view(B, D, 1) + errors # B K D @ B D . + B K 1 -> B K 1
 
         return xs, ys
-    
+
+
     def loop_batches(self, num_examples: int, batch_size: int):
         """
         Iterate over batches of synthetic data (token sequences) for
@@ -100,6 +103,7 @@ class RegressionSequenceDistribution(Generic[T]):
                 num_examples=num_examples,
                 batch_size=batch_size,
             )
+
 
     def to(self, device: str):
         self.task_distribution.to(device)
@@ -132,6 +136,7 @@ class TaskDistribution:
         self.task_size = task_size
         self.device = device
 
+
     def sample_tasks(self, n: int):
         """
         produce a sample of `n` tasks from the concrete task distribution
@@ -147,7 +152,8 @@ class TaskDistribution:
             the `n` tasks, each as one row of a 2d tensor.
         """
         return NotImplemented
-    
+
+
     def to(self, device: str):
         """
         Move this task distribution to a different device
@@ -195,20 +201,13 @@ class DiscreteTaskDistribution(TaskDistribution):
     def __init__(self, task_size: int, num_tasks: int, device='cpu'):
         super().__init__(task_size=task_size, device=device)
         self.num_tasks = num_tasks
-        self.generator = torch.Generator(device=self.device)
-
-    def sample_task(self, idx: int):
-        """
-        Sample the task with id `idx` from the task distribution.
-        """
-        self.generator.manual_seed(idx)
-        return torch.normal(
+        self.tasks = torch.normal(
             mean=0.,
             std=1.,
-            size=(self.task_size,),
-            generator=self.generator,
+            size=(self.num_tasks, self.task_size,),
             device=self.device,
         )
+
 
     def sample_tasks(self, n: int):
         """
@@ -231,12 +230,7 @@ class DiscreteTaskDistribution(TaskDistribution):
             size=(n,),
             device=self.device,
         )
-        # TODO: See if there are better (e.g., parallel) ways of doing this. 
-        # TODO: Compare against just generating the dataset ahead of time and reading from disk.
-        return torch.stack([
-            self.sample_task(int(i))
-            for i in task_selection
-        ])
+        return self.tasks[task_selection]
 
 
     def to(self, device: str):
@@ -252,8 +246,9 @@ class DiscreteTaskDistribution(TaskDistribution):
 
         * `self`
         """
-        self.generator = torch.Generator(device=device)
+        self.tasks = self.tasks.to(device)
         return super().to(device)
+
 
 class GaussianTaskDistribution(TaskDistribution):
     """