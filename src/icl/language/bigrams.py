31c02a3897b412a7dda195401b6e6ad9b3a88aa3
Jesse Hoogland <jessequinten@gmail.com>
2024-02-23 14:27:05 -0800

    ADD saving/retrieving Mistral checkpoints from AWS

--- src/icl/language/bigrams.py
+++ src/icl/language/bigrams.py
@@ -9,12 +9,12 @@ import tqdm
 from icl.constants import BIGRAMS_FILEPATH
 from icl.monitoring import stdlogger
 
-AWS_BUCKET_NAME = os.getenv('AWS_BUCKET_NAME')
+AWS_LANGUAGE_BUCKET_NAME = os.getenv('AWS_LANGUAGE_BUCKET_NAME', os.getenv('AWS_BUCKET_NAME'))
 
 def get_bigrams(file_path=BIGRAMS_FILEPATH)    
     if not os.path.exists(file_path): 
         client = boto3.client('s3')
-        client.download_file(AWS_BUCKET_NAME, 'other/language/bigram_freq_percents.pkl', file_path)
+        client.download_file(AWS_LANGUAGE_BUCKET_NAME, 'other/language/bigram_freq_percents.pkl', file_path)
     
     with open(file_path, 'rb') as file:
         return pickle.load(file)
